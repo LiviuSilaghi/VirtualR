a75299063602965f94f055e323ab05e270ba6b10
LiviuSilaghi <szilagyi.liviu@gmail.com>
2025-06-05 21:02:27 +0100

    HeroSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 /** @format */
 
+import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
 
 export const App = () => {
   return (
     <div>
       <Navbar />
+      <div className="max-w-7x1 mx-auto pt-20 px-6">
+        <HeroSection />
+      </div>
     </div>
   );
 };
